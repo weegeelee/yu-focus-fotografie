03f5f96e97d01e06026e3e5016af6ae1b28d0ad5
Yu Chen <weegeelee@hotmail.com>
2025-03-31 22:07:18 +0200

    delete folder

--- src/supaConfig.js
+++
@@ -1,8 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://skypcroncrknshvbexyi.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase;
