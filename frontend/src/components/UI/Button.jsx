4de6dc2c0521eccfc2c77351c2e569fcf3669159
Yu Chen <weegeelee@hotmail.com>
2025-04-12 15:17:06 +0200

    change gh-pages

--- frontend/src/components/UI/Button.jsx
+++
@@ -1,5 +0,0 @@
-export default function Button({children, textOnly, className}) {
-    let cssClass = textOnly ? 'text-button'  : 'button';
-    cssClass += ' ' + className
-    return <button className={cssClass}>{children}</button>
-}
